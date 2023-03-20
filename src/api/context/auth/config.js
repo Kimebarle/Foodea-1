0b74350b7ba75a295e729934c50443acc66d379a
Christopher Calleja <calleja.christopher.bscs2019@gmail.com>
2023-03-21 00:10:23 +0800

    fixed some issues on signup

--- src/api/context/auth/config.js
+++ src/api/context/auth/config.js
@@ -1,4 +1,7 @@
 export const BASE_URL = "https://foodea-website.herokuapp.com/api/v1/";
 
+export const BASE_URL_RECOMMENDATION =
+  "https://foodea-recommendations.herokuapp.com";
+
 export const apiKey =
   "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";
