c15892c693c985721e7425bea411547ec17edfd3
Christopher Calleja <calleja.christopher.bscs2019@gmail.com>
2023-03-30 16:57:53 +0800

    Update ActivityLogDetails.js

--- src/views/screens/BottomNavigation/ActivityLogDetails.js
+++ src/views/screens/BottomNavigation/ActivityLogDetails.js
@@ -22,153 +22,151 @@ import Constants from "expo-constants";
 const ActivityLogDetails = ({ navigation, route }) => {
   const { itemValue } = route.params;
 
-const ActivityLogDetails = ({ navigation }) => {
-
+  const ActivityLogDetails = ({ navigation }) => {
     function renderHeader() {
-        return (
-            <Header
-                containerStyle={{
-                    height: 80,
-                    marginHorizontal: SIZES.padding,
-                    alignItems: "center",
+      return (
+        <Header
+          containerStyle={{
+            height: 80,
+            marginHorizontal: SIZES.padding,
+            alignItems: "center",
+          }}
+          title={"Activity Details"}
+          leftComponent={
+            // Open Custom Drawer
+            <TouchableOpacity
+              style={{
+                width: 40,
+                height: 40,
+                alignItems: "center",
+                justifyContent: "center",
+                borderWidth: 1,
+                borderColor: COLORS.gray2,
+                borderRadius: SIZES.radius,
+              }}
+              onPress={() => {
+                console.log(Constants.statusBarHeight);
+              }}
+            >
+              <Image
+                source={icons.backarrow}
+                style={{
+                  borderRadius: SIZES.radius,
+                  color: COLORS.gray2,
                 }}
-                title={"Activity Details"}
-                leftComponent={
-                    // Open Custom Drawer
-                    <TouchableOpacity
-                        style={{
-                            width: 40,
-                            height: 40,
-                            alignItems: "center",
-                            justifyContent: "center",
-                            borderWidth: 1,
-                            borderColor: COLORS.gray2,
-                            borderRadius: SIZES.radius,
-                        }}
-                        onPress={() => {console.log(Constants.statusBarHeight)}}
-                    >
-                        <Image
-                            source={icons.backarrow}
-                            style={{
-                                borderRadius: SIZES.radius,
-                                color: COLORS.gray2,
-                            }}
-                        />
-                    </TouchableOpacity>
-                }
-                rightComponent={
-                    <View
-                        style={{
-                            width: 40,
-                        }}
-                    ></View>
-                }
-            />
-        );
+              />
+            </TouchableOpacity>
+          }
+          rightComponent={
+            <View
+              style={{
+                width: 40,
+              }}
+            ></View>
+          }
+        />
+      );
     }
 
     function renderCartList() {
-        return (
-            <FlatList
-                data={myCartList}
-                keyExtractor={(item) => `${item.id}`}
-                contentContainerStyle={{
-                    marginTop: SIZES.radius,
-                    paddingHorizontal: SIZES.padding,
-                    paddingBottom: SIZES.padding,
+      return (
+        <FlatList
+          data={myCartList}
+          keyExtractor={(item) => `${item.id}`}
+          contentContainerStyle={{
+            marginTop: SIZES.radius,
+            paddingHorizontal: SIZES.padding,
+            paddingBottom: SIZES.padding,
+          }}
+          renderItem={(data) => (
+            <View
+              style={{
+                height: 100,
+                backgroundColor: COLORS.lightGray2,
+                ...styles.cartItemContainer,
+              }}
+            >
+              {/*   Food Image */}
+              <View
+                style={{
+                  width: 90,
+                  height: 100,
+                  marginLeft: -10,
                 }}
-                renderItem={(data) => (
-                    <View
-                        style={{
-                            height: 100,
-                            backgroundColor: COLORS.lightGray2,
-                            ...styles.cartItemContainer,
-                        }}
-                    >
-                        {/*   Food Image */}
-                        <View
-                            style={{
-                                width: 90,
-                                height: 100,
-                                marginLeft: -10,
-                            }}
-                        >
-                            <Image
-                                source={{ }}
-                                resizeMode="contain"
-                                style={{
-                                    width: "100%",
-                                    height: "100%",
-                                    position: "absolute",
-                                    top: 1,
-                                }}
-                            />
-                        </View>
+              >
+                <Image
+                  source={{}}
+                  resizeMode="contain"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 1,
+                  }}
+                />
+              </View>
 
-                        {/*   Food Info */}
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: "row",
-                            }}
-                        >
-                            <Text style={{ width: 150, ...FONTS.h5, fontSize: 13 }}>
-                                Product Name
-                                {"\n"}
-                                <Text
-                                    style={{
-                                        color: COLORS.primary,
-                                        ...FONTS.h3,
-                                    }}
-                                >
-                                    ₱ Price
-                                </Text>
-                                {"\n"}
-                                <Text
-                                    style={{
-                                        color: COLORS.gray,
-                                        ...FONTS.h5,
-                                    }}
-                                >
-                                    {" "}
-                                    calories
-                                </Text>
-                                {"\n"}
-                                <Text
-                                    style={{
-                                        color: COLORS.gray,
-                                        ...FONTS.h5,
-                                    }}
-                                >
-                                    Quantity
-                                </Text>
-                            </Text>
-                        </View>
-                    </View>
-                )}
-            />
-        );
+              {/*   Food Info */}
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                }}
+              >
+                <Text style={{ width: 150, ...FONTS.h5, fontSize: 13 }}>
+                  Product Name
+                  {"\n"}
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      ...FONTS.h3,
+                    }}
+                  >
+                    ₱ Price
+                  </Text>
+                  {"\n"}
+                  <Text
+                    style={{
+                      color: COLORS.gray,
+                      ...FONTS.h5,
+                    }}
+                  >
+                    {" "}
+                    calories
+                  </Text>
+                  {"\n"}
+                  <Text
+                    style={{
+                      color: COLORS.gray,
+                      ...FONTS.h5,
+                    }}
+                  >
+                    Quantity
+                  </Text>
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      );
     }
+  };
 
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        top: Constants.statusBarHeight - 32,
+      }}
+    >
+      {/* Header */}
+      {renderHeader()}
 
-
-
-    return (
-        <View style={{
-            
-            alignItems: 'center',
-            top: Constants.statusBarHeight - 32,
-        }}>
-            {/* Header */}
-            {renderHeader()}
-            
-            {/* CartList */}
-            {renderCartList()}
-        </View>
-    )
-}
-
-
+      {/* CartList */}
+      {renderCartList()}
+    </View>
+  );
+};
 
 export default ActivityLogDetails;
 
